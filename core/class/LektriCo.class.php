5f9c6db74f49ed5df9d9335f649c0ec82242f507
Sébastien Spangenberger <52532115+Sattaz@users.noreply.github.com>
2024-01-28 13:11:36 +0100

    Update LektriCo.class.php

--- core/class/LektriCo.class.php
+++ core/class/LektriCo.class.php
@@ -217,6 +217,18 @@ class LektriCo extends eqLogic {
 					$state = 'Charge.Stop';
 					break;              
 			}
+
+            curl_setopt_array($ch, [
+               	CURLOPT_URL => 'http://'.$LektriCo_IP.'/rpc/Charging_schedule.Override',
+  				CURLOPT_RETURNTRANSFER => true,
+  				CURLOPT_ENCODING => "",
+  				CURLOPT_MAXREDIRS => 10,
+  				CURLOPT_TIMEOUT => 10,
+  				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  				CURLOPT_CUSTOMREQUEST => 'GET',
+  				CURLOPT_HTTPHEADER => ['Content-Type: application/json'],
+            ]);
+		$response = curl_exec($ch);
               
             curl_setopt_array($ch, [
                	CURLOPT_URL => 'http://'.$LektriCo_IP.'/rpc/'.$state,
